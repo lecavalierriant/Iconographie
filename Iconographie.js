a55b41cbe1b71328e15d8fbe0fc3519f2e5c2b6c
𝔄𝔯𝔱𝔲𝔰 𝔡𝔢 ℭ𝔥𝔞𝔳𝔞𝔤𝔫𝔞𝔠 <lecavalierriant@gmail.com>
2025-04-01 12:27:37 +0200

    maj

--- Iconographie.js
+++ Iconographie.js
@@ -10,30 +10,28 @@ document.addEventListener(
 		éléments = document.querySelectorAll("table");
 		total = éléments.length;
 		occurrences = {};
-		nombreDeClasses = 0;
 		éléments.forEach(
 			function (élément) {
 				classe = élément.getAttribute("class");
-				if (classe) {
-					if (!occurrences[classe]) {occurrences[classe] = 1;} else {occurrences[classe]++;}
-					nombreDeClasses++;
-				}
+				if (classe) {if (!occurrences[classe]) {occurrences[classe] = 1;} else {occurrences[classe]++;}}
 			}
 		);
-		tableLégende = document.getElementById("table-légende");
-		tableLégende.insertRow().insertCell(0).outerHTML = "<th>Classe</th><th>Occurrences</th><th>Proportion</th>";
-		for (classe in occurrences) {nouvelleLigneLégende(classe, occurrences[classe], total);}
-		nouvelleLigneLégende("table-Total", total, total);
+		tableau = document.getElementById("table-légende");
+		tableau.insertRow().insertCell(0).outerHTML = "<th>Classe</th><th>Occurrences</th><th>Proportion</th>";
+		for (classe in occurrences) {nouvelleLigne(classe, occurrences[classe], total);}
+		nouvelleLigne("table-Total", total, total);
 	}
 );
 
-function nouvelleLigneLégende(classe, occurrences, total) {
-	petiteClasse = classe.split("-")[1];
-	pourcentage = (occurrences / total) * 100;
-	ligne = tableLégende.insertRow();
-	ligne.insertCell(0).outerHTML = "<th><table class = '" + classe + "'><td>" + petiteClasse + "</td></table><th>";
+function nouvelleLigne(classe, occurrences, total) {
+	nomClasse = classe.split("-")[1];
+	ligne = tableau.insertRow();
+	ligne.insertCell(0).outerHTML =
+		"<th><table class = '" + classe + "'><td>" +
+			nomClasse.charAt(0).toUpperCase() + nomClasse.slice(1) +
+		"</td></table><th>";
 	ligne.insertCell(1).textContent = occurrences;
-	ligne.insertCell(2).textContent = pourcentage.toFixed(1).replace(".", ",") + " %";
+	ligne.insertCell(2).textContent = ((occurrences / total) * 100).toFixed(1).replace(".", ",") + " %";
 }
 
 // ================================================================================================================= //
